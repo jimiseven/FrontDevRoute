b10935c31a6fff032d4e6d38b4942fac2cb91776
Jimi Joe <tano777864@gmail.com>
2024-01-15 15:12:09 -0400

    iteracion de ngfor, practicar sintaxis de js

--- todoList/src/app/pages/home/home.component.ts
+++ todoList/src/app/pages/home/home.component.ts
@@ -16,5 +16,16 @@ export class HomeComponent {
     'add 2',
     'add 3',
     'de computadores'
-  ])
+  ]);
+
+
+  changeHandler(event: Event){
+    const input = event.target as HTMLInputElement;
+    const newTask = input.value;
+    this.tasks.update((tasks) => [...tasks, newTask]);
+  }
+
+  deleteTask(index: number){
+    this.tasks.update((tasks) => tasks.filter((task, position) => position !== index));
+  }
 }
