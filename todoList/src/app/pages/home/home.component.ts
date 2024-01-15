660ec90cd3bcd505429daff44d0d605c1f10f8f0
Jimi Joe <tano777864@gmail.com>
2024-01-15 14:27:14 -0400

    directivas de control

--- todoList/src/app/pages/home/home.component.ts
+++ todoList/src/app/pages/home/home.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  tasks = signal([
+    'Lista de: ',
+    'Componentes',
+    'add 1',
+    'add 2',
+    'add 3',
+    'de computadores'
+  ])
 }
--- todoList/src/app/pages/labs/labs.component.ts
+++ todoList/src/app/pages/labs/labs.component.ts
@@ -1,20 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { signal } from '@angular/core';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-LabsComponentmponent',
+  selector: 'app-LabsComponent',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './labs.component.html',
   styleUrl: './labs.component.css'
 })
 export class LabsComponent {
   title = 'bienvenido';//este dato lo estamos pasando atraves del html
-  task = [
+  tasks = signal([
     'Lista de: ',
     'Componentes',
     'de computadores'
-  ]
+  ])
   name = signal('jimi joe');
   age = 18;
 
