8d565bee792d57f00dc0863da57c0571afc5e260
Jimi Joe <tano777864@gmail.com>
2024-01-16 15:49:35 -0400

    solucion al error de ingresar y eliminar datos

--- todoList/src/app/pages/home/home.component.ts
+++ todoList/src/app/pages/home/home.component.ts
@@ -1,46 +1,52 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
-import { title } from 'process';
+//import { title } from 'process';
 
-
-import { Task } from '../../models/task.model';
+//import { Task } from '../../models/task.model';
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-  tasks = signal <Task[]>([
-    {
-      id: Date.now(),
-      title : 'crear proyecto',
-      completed : false
-    },
-    {
-      id: Date.now(),
-      title : 'crear proyecto 2',
-      completed : true
-    }
+  tasks = signal([
+    'tarea a',
+    'tarea b',
+    'tarea c',
+    'tarea d',
+    'tarea e',
+    // {
+    //   id: Date.now(),
+    //   title : 'crear proyecto',
+    //   completed : false
+    // },
+    // {
+    //   id: Date.now(),
+    //   title : 'crear proyecto 2',
+    //   completed : true
+    // }
   ]);
 
-
-  changeHandler(event: Event){
+  changeHandler(event: Event) {
     const input = event.target as HTMLInputElement;
     const newTask = input.value;
-    this.addTaks(newTask);
-  }
-
-  addTaks(Title: string){
-    const newTask = {
-      id : Date.now(),
-      title,
-      completed: false,
-    };
+    //this.addTaks(newTask);
     this.tasks.update((tasks) => [...tasks, newTask]);
   }
-  deleteTask(index: number){
-    this.tasks.update((tasks) => tasks.filter((task, position) => position !== index));
+
+  // addTaks(Title: string){
+  //   const newTask = {
+  //     id : Date.now(),s
+  //     title,
+  //     completed: false,
+  //   };
+  //   this.tasks.update((tasks) => [...tasks, newTask]);
+  // }
+  deleteTask(index: number) {
+    this.tasks.update((tasks) =>
+      tasks.filter((task, position) => position !== index)
+    );
   }
 }
