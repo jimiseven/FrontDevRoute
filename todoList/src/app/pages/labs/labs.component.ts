469bd7601128b45f9a0b7cf85d397ceb82815967
Jimi Joe <tano777864@gmail.com>
2024-01-23 12:33:08 -0400

    uso de clases y funciones especiales en agular

--- todoList/src/app/pages/labs/labs.component.ts
+++ todoList/src/app/pages/labs/labs.component.ts
@@ -1,54 +1,59 @@
 import { CommonModule } from '@angular/common';
 import { signal } from '@angular/core';
 import { Component } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';//importacion del modulo
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms'; //importacion del modulo
 import { ColdObservable } from 'rxjs/internal/testing/ColdObservable';
 
 @Component({
   selector: 'app-LabsComponent',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],//uso del modulo
+  imports: [CommonModule, ReactiveFormsModule], //uso del modulo
   templateUrl: './labs.component.html',
-  styleUrl: './labs.component.css'
+  styleUrl: './labs.component.css',
 })
 export class LabsComponent {
-  title = 'bienvenido';//este dato lo estamos pasando atraves del html
-  tasks = signal([
-    'Lista de: ',
-    'Componentes',
-    'de computadores'
-  ])
+  title = 'bienvenido'; //este dato lo estamos pasando atraves del html
+  tasks = signal(['Lista de: ', 'Componentes', 'de computadores']);
   name = signal('jimi joe');
   age = 18;
 
-  person = signal ({
-    name : 'joe',
-    age : 16
-  })
+  person = signal({
+    name: 'joe',
+    age: 16,
+  });
 
-  colorCtrl = new FormControl();//paquete de angular forms
+  colorCtrl = new FormControl(); //paquete de angular forms
   widthCtrl = new FormControl(50, {
-    nonNullable : true
-  });//paquete de angular forms
+    nonNullable: true,
+  }); //paquete de angular forms
 
-  constructor(){
-    this.colorCtrl.valueChanges.subscribe(value =>{
+  nameCtrl = new FormControl(50, {
+    nonNullable: true,
+    validators: [
+      Validators.required,
+      Validators.minLength(3)
+    ]
+  }); //paquete de angular forms
+
+
+  constructor() {
+    this.colorCtrl.valueChanges.subscribe((value) => {
       console.log(value);
     });
   }
 
-  clickHandler(){
-    alert('hola!')
+  clickHandler() {
+    alert('hola!');
   }
 
-  changeHandler(event: Event){
+  changeHandler(event: Event) {
     const input = event.target as HTMLInputElement;
     const newValue = input.value;
     this.name.set(newValue);
   }
 
-  keydownHandler(event: KeyboardEvent){
+  keydownHandler(event: KeyboardEvent) {
     const input = event.target as HTMLInputElement;
-    console.log(input.value)
+    console.log(input.value);
   }
 }
