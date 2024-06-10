7b6adc37dd77af8bdb91f01d7189ea28071a6415
Jimi Joe <tano777864@gmail.com>
2024-06-10 12:40:10 -0400

    usamos el audio y le pusimos un botton play/apuse

--- todoList/src/app/domains/info/components/wave-audio/wave-audio.component.ts
+++ todoList/src/app/domains/info/components/wave-audio/wave-audio.component.ts
@@ -1,11 +1,12 @@
-import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 
 import WaveSurfer from 'wavesurfer.js';//importacion de la libreria
 @Component({
   selector: 'app-wave-audio',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './wave-audio.component.html',
   styleUrl: './wave-audio.component.css'
 })
@@ -13,13 +14,21 @@ export class WaveAudioComponent {
 
   @Input({required: true}) audioUrl!: string; // audioUrl: string = ''
   @ViewChild('wave') container!: ElementRef;
+  private ws!:WaveSurfer;
+  isPlaying = signal(false);
 
   ngAfterViewInit(): void {
     //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
     //Add 'implements AfterViewInit' to the class.
-    WaveSurfer.create({
+    this.ws = WaveSurfer.create({
       url:this.audioUrl,
       container: this.container.nativeElement//document.getElementById 'en js', nosotros lo hicimos modo angular
     })
+    this.ws.on('play', () => this.isPlaying.set(true));
+    this.ws.on('pause', () => this.isPlaying.set(false));
+  }
+
+  playPause(){
+    this.ws.playPause();
   }
 }
