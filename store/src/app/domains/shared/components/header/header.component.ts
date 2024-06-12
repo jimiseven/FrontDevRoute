5355632cb248ee9e3f3f665aed6c19257c362f70
Jimi Joe <tano777864@gmail.com>
2024-06-12 10:45:37 -0400

    suma del total sin problemas de rendimiento v45

--- store/src/app/domains/shared/components/header/header.component.ts
+++ store/src/app/domains/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, input, signal } from '@angular/core';
+import { Component, Input, SimpleChanges, input, signal } from '@angular/core';
 import { Product } from '../../../../models/product.model';
 import { CommonModule } from '@angular/common';
 
@@ -12,8 +12,21 @@ import { CommonModule } from '@angular/common';
 export class HeaderComponent {
   hideSideMenu = signal(true);
   @Input({required: true}) cart: Product[] = [];
+  total = signal(0);
 
   toogleSideMenu(){
     this.hideSideMenu.update(prevState => !prevState);
+  }
+  ngOnChanges(changes: SimpleChanges) {
+    //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+    //Add '${implements OnChanges}' to the class.
+    const cart = changes['cart'];
+    if(cart)[
+      this.total.set(this.calTot())
+    ]
+  }
+
+  calTot(){
+    return this.cart.reduce((total, product) => total + product.price, 0);
   }
 }
