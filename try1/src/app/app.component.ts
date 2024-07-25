83ffb36c16fe4de235af28a39b537e739026de37
LaptopTarks <tano777864@gmail.com>
2024-07-24 20:48:20 -0400

    configuracion basica

--- try1/src/app/app.component.ts
+++ try1/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TodoComponent } from "./pages/todo/todo.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, TodoComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'try1';
