708a1129275ba34821858ff39d167afdb207ad6b
Jimi Joe <tano777864@gmail.com>
2024-07-28 15:05:46 -0400

    appJimi - manipulacion del doom

--- appJimi/src/app/heroes/listado/listado.component.ts
+++ appJimi/src/app/heroes/listado/listado.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrl: './listado.component.css'
 })
 export class ListadoComponent {
+  heroes: string [] = ['name1', 'name3', 'name4', 'name5', 'name6']
+  heroeBorrado : string = '';
+
+  borrarHeroe(){
+    if (this.heroes.length > 0) {
+      this.heroeBorrado = this.heroes.pop() || '';
+      // console.log(heroeBorrado);
+    } else {
+      console.log('No hay héroes para eliminar');
+    }
+  }
 
 }
