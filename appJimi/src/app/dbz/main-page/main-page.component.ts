0ed8e15601b6af84c22bc08aad9a2603b3598140
Jimi Joe <tano777864@gmail.com>
2024-07-31 22:00:28 -0400

    appJimi v6 - uso de ngSubmit

--- appJimi/src/app/dbz/main-page/main-page.component.ts
+++ appJimi/src/app/dbz/main-page/main-page.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrl: './main-page.component.css'
 })
 export class MainPageComponent {
 
+  // //primera manera o manera JS
+  // metodoName1( event:any ){
+  //   event.preventDefault();
+  //   console.log('key');
+  // }
+
+
+  //manera angular
+  metodoName1(){
+    console.log('hola hola')
+  }
 }
