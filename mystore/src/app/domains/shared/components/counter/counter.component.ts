9d924d6fb04fe2352f8475b40ae5d6f44c34f68a
Jimi Joe <tano777864@gmail.com>
2024-08-14 10:22:08 -0400

    mystore v36 - cuidado con ngSkipHydration y uso de ngonchanges & ngoninit

---
+++ mystore/src/app/domains/shared/components/counter/counter.component.ts
@@ -0,0 +1,36 @@
+import { Component, Input, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  standalone: true,
+  imports: [],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.css'
+})
+export class CounterComponent {
+  @Input({required:true}) duration = 0;
+  @Input({required:true}) message = '';
+
+  contructor(){//no es asincronico
+
+    console.log('contructor');
+    console.log('-'.repeat(10));
+  }
+
+
+  ngOnChanges(changes: SimpleChanges): void {
+    //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+    //Add '${implements OnChanges}' to the class.
+    console.log('ngOnChanges');
+    console.log('-'.repeat(10));
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    console.log('ngOnInit');
+    console.log('-'.repeat(10));
+    console.log('duration ->', this.duration);
+    console.log('message ->', this.message);
+  }
+}
