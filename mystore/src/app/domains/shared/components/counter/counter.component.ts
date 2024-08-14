b7334b75539aa8f10b498800359e6001b2a614fb
Jimi Joe <tano777864@gmail.com>
2024-08-14 11:16:34 -0400

    mystore -39 - tener cuidado con hidratacion, y cuidado con el ngoninit

--- mystore/src/app/domains/shared/components/counter/counter.component.ts
+++ mystore/src/app/domains/shared/components/counter/counter.component.ts
@@ -1,15 +1,19 @@
-import { Component, Input, SimpleChanges } from '@angular/core';
+import { Component, Input, signal, SimpleChanges } from '@angular/core';
+import { ProductComponent } from "../../../products/components/product/product.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-counter',
   standalone: true,
-  imports: [],
+  host: {ngSkipHydration: 'true'},
+  imports: [ProductComponent, CommonModule],
   templateUrl: './counter.component.html',
   styleUrl: './counter.component.css'
 })
 export class CounterComponent {
   @Input({required:true}) duration = 0;
   @Input({required:true}) message = '';
+  counter = signal(0);
 
   contructor(){//no es asincronico
 
@@ -18,11 +22,18 @@ export class CounterComponent {
   }
 
 
-  ngOnChanges(changes: SimpleChanges): void {
+  ngOnChanges(changes: SimpleChanges) {
     //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
     //Add '${implements OnChanges}' to the class.
     console.log('ngOnChanges');
     console.log('-'.repeat(10));
+    console.log(changes);
+    const duration = changes['duration'];
+    console.log(duration);
+    if (duration){
+      this.doAlgo();
+    }
+
   }
 
   ngOnInit(): void {
@@ -32,5 +43,27 @@ export class CounterComponent {
     console.log('-'.repeat(10));
     console.log('duration ->', this.duration);
     console.log('message ->', this.message);
+    window.setInterval(() => {
+      console.log('run interval')
+      this.counter.update(statePrev => statePrev + 1)
+    }, 1000);
+  }
+
+  ngAfterViewInit(): void {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+    console.log('ngafteron init');
+    console.log('-'.repeat(10));
+  }
+
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    console.log('ng destroy');
+    console.log('-'.repeat(10));
+  }
+
+  doAlgo() {
+    console.log('change duration')
   }
 }
