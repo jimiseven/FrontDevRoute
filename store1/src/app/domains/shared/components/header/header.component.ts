74da036a463391941e0e37529632cefe57f6bd15
LaptopTarks <tano777864@gmail.com>
2024-10-09 19:08:24 -0400

    evitando problemas de rendimeientos con ngOnChanges

--- store1/src/app/domains/shared/components/header/header.component.ts
+++ store1/src/app/domains/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, signal } from '@angular/core';
+import { Component, Input, SimpleChanges, signal } from '@angular/core';
 import { Product } from '../../models/product.model';
 import { CommonModule } from '@angular/common';
 
@@ -11,8 +11,23 @@ import { CommonModule } from '@angular/common';
 })
 export class HeaderComponent {
   hideMenu = signal(true);
+  tot = signal(0);
+
   @Input({required : true}) cart: Product[] = []; 
   toogleSideMenu(){
     this.hideMenu.update(prevState => !prevState)
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+    //Add '${implements OnChanges}' to the class.
+    const cart = changes['cart'];
+    if(cart){
+      this.tot.set(this.calcTol());
+    }
+  }
+
+  calcTol(){
+    return this.cart.reduce((total, product) =>total + product.price, 0);
   }
 }
